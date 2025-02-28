0a6091218cc7f80d2f6575f672b329b145593941
Nazar Faustyn <153837991+Faustynn@users.noreply.github.com>
2025-02-27 16:42:52 -0800

    Start Subjects reprez.

--- src/main/java/org/main/unimap_pc/client/configs/AppConfig.java
+++ src/main/java/org/main/unimap_pc/client/configs/AppConfig.java
@@ -6,7 +6,9 @@ import lombok.Setter;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
 
 // TODO: Rewrite prop file with lombok
@@ -166,8 +168,15 @@ public class AppConfig {
 
 
     public static Image getAvatar(String avatarID) {
-        String imagePath = "../images/avatares/" + avatarID + ".png";
-        return new Image(imagePath);
+        String imagePath = "/org/main/unimap_pc/images/avatares/" + avatarID + ".png";
+        System.out.println(imagePath);
+
+        URL resource = AppConfig.class.getResource(imagePath);
+        if (resource == null) {
+            System.err.println("Avatar image not found: " + imagePath);
+            return null; // or return a default image
+        }
+
+        return new Image(resource.toExternalForm());
     }
 }
-
